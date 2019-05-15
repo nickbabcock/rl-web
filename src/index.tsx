4fee542a1880e7a02468649d034dcc674828fac8
Nick Babcock <nbabcock19@hotmail.com>
2019-05-15 16:55:22 -0500

    Implement drag and drop

--- src/index.tsx
+++ src/index.tsx
@@ -21,4 +21,44 @@ function newReplay(replay: File) {
   reader.readAsArrayBuffer(replay);
 }
 
+const dragHoverElement = document.getElementById("drag-hover")!;
+function dragDrop(ev: DragEvent) {
+  ev.preventDefault();
+  dragHoverElement.style.display = "none";
+
+  if (ev.dataTransfer && ev.dataTransfer.items) {
+    const items = ev.dataTransfer.items;
+    if (items.length !== 1) {
+      throw Error("unexpected one file drop");
+    }
+    const file = items[0].getAsFile();
+    if (file === null) {
+      throw Error("bad dropped file");
+    }
+
+    newReplay(file);
+  } else if (ev.dataTransfer && ev.dataTransfer.files) {
+    const files = ev.dataTransfer.files;
+    if (files.length !== 1) {
+      throw Error("unexpected one file drop");
+    }
+    newReplay(files[0]);
+  } else {
+    throw Error("unexpected data transfer");
+  }
+}
+
+function dragOverHandler(ev: Event) {
+  ev.preventDefault();
+  dragHoverElement.style.display = "inherit";
+}
+
+function dragLeaveHandler() {
+  dragHoverElement.style.display = "none";
+}
+
+document.addEventListener("drop", dragDrop, false);
+document.addEventListener("dragover", dragOverHandler, false);
+document.addEventListener("dragleave", dragLeaveHandler, false);
+
 render(<App newReplay={newReplay} />, mainElement, appElement);
