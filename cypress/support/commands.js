13e88f83e2a5240ec94b3d717165f4d00d0d1e80
Nick Babcock <nbabcock19@hotmail.com>
2019-12-10 18:36:29 -0600

    Use cypress-file-upload

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -1,43 +1,12 @@
-/**
- * Converts Cypress fixtures, including JSON, to a Blob. All file types are
- * converted to base64 then converted to a Blob using Cypress
- * expect application/json. Json files are just stringified then converted to
- * a blob (prevents issues with invalid string decoding).
- * @param {String} fileUrl - The file url to upload
- * @param {String} type - content type of the uploaded file
- * @return {Promise} Resolves with blob containing fixture contents
- */
-function getFixtureBlob(fileUrl, type) {
-  return type === 'application/json'
-    ? cy
-        .fixture(fileUrl)
-        .then(JSON.stringify)
-        .then(jsonStr => new Blob([jsonStr], { type: 'application/json' }))
-    : cy.fixture(fileUrl, 'base64').then(Cypress.Blob.base64StringToBlob)
-}
+import "cypress-file-upload";
 
-/**
- * Uploads a file to an input
- * @memberOf Cypress.Chainable#
- * @name uploadFile
- * @function
- * @param {String} selector - element to target
- * @param {String} fileUrl - The file url to upload
- * @param {String} type - content type of the uploaded file
- */
-Cypress.Commands.add('uploadFile', (selector, fileUrl, type = '') => {
-  return cy.get(selector).then(subject => {
-    return getFixtureBlob(fileUrl, type).then(blob => {
-      return cy.window().then(win => {
-        const el = subject[0]
-        const nameSegments = fileUrl.split('/')
-        const name = nameSegments[nameSegments.length - 1]
-        const testFile = new win.File([blob], name, { type })
-        const dataTransfer = new win.DataTransfer()
-        dataTransfer.items.add(testFile)
-        el.files = dataTransfer.files
-        return subject
-      })
-    })
-  })
-})
+Cypress.Commands.add("uploadFile", (selector, fileUrl, type = "") => {
+  return cy.fixture(fileUrl, "base64").then(fileContent => {
+    cy.get(selector).upload({
+      fileContent,
+      fileName: fileUrl,
+      mimeType: "application/octet-stream",
+      encoding: "base64"
+    });
+  });
+});
