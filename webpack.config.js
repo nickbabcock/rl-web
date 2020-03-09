7f8dcf4f717a53559550f86bbdbdd7062575bd45
Nick Babcock <nbabcock19@hotmail.com>
2020-03-09 07:10:42 -0500

    webpack

---
+++ webpack.config.js
@@ -0,0 +1,17 @@
+const path = require('path');
+
+module.exports = {
+  entry: './src/index.tsx',
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: 'ts-loader',
+        exclude: /node_modules/,
+      },
+    ],
+  },
+  resolve: {
+    extensions: [ '.tsx', '.ts', '.js' ],
+  },
+};
