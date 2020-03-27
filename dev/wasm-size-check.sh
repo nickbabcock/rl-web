6d84af9205c071d051500253cbe6ecb06937a9a0
Nick Babcock <nbabcock19@hotmail.com>
2020-03-27 07:25:13 -0500

    Fix wasm check

--- dev/wasm-size-check.sh
+++ dev/wasm-size-check.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-bytes=$(docker run --rm -i "$(docker build -q .)" find /usr/share/nginx/html -iname rl_wasm_bg-*.wasm -exec wc -c {} \; | cut -d " " -f1)
+bytes=$(docker run --rm -i "$(docker build -q .)" find /usr/share/nginx/html -iname *.wasm -exec wc -c {} \; | cut -d " " -f1)
 [[ $bytes -lt 1000 ]] && echo "wasm generated incorrectly" && exit 1
 echo "done"
