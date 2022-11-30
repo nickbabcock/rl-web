3ff889df4bbe509141131caed20f93072e9564a3
Nick Babcock <nbabcock19@hotmail.com>
2022-11-30 06:14:16 -0600

    Migrate security headers to next config

--- next.config.js
+++ next.config.js
@@ -1,3 +1,37 @@
+const isDev = process.env.NODE_ENV !== "production";
+
+const csp = `
+  default-src 'none';
+  connect-src 'self';
+  img-src 'self';
+  script-src 'self' 'wasm-unsafe-eval' ${isDev ? "'unsafe-eval'" : ""};
+  style-src 'self' ${isDev ? "'unsafe-inline'" : ""};
+  worker-src 'self'
+`;
+
+const securityHeaders = [
+  {
+    key: "X-Content-Type-Options",
+    value: "nosniff",
+  },
+  {
+    key: "X-Frame-Options",
+    value: "SAMEORIGIN",
+  },
+  {
+    key: "Strict-Transport-Security",
+    value: "max-age=8640000",
+  },
+  {
+    key: "Referrer-Policy",
+    value: "no-referrer",
+  },
+  {
+    key: "Content-Security-Policy",
+    value: csp.replace(/\s{2,}/g, " ").trim(),
+  },
+];
+
 /** @type {import('next').NextConfig} */
 module.exports = {
   reactStrictMode: true,
@@ -11,10 +45,18 @@ module.exports = {
     config.module.rules.push({
       test: /\.(wasm|replay)$/,
       resourceQuery: {
-        not: /module/
+        not: /module/,
       },
       type: "asset/resource",
     });
     return config;
-  }
-}
+  },
+  async headers() {
+    return [
+      {
+        source: "/:path*",
+        headers: securityHeaders,
+      },
+    ];
+  },
+};
