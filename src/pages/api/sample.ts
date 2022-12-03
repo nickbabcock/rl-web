5f7f864cefaa7f6197c65419219bc3722839e512
Nick Babcock <nbabcock19@hotmail.com>
2022-12-02 21:54:35 -0600

    Fix server side includes with static analysis of additional files

--- src/pages/api/sample.ts
+++ src/pages/api/sample.ts
@@ -2,13 +2,12 @@ import fs from "fs/promises";
 import { ReplayParser } from "@/features/worker/ReplayParser";
 import init, * as RlMod from "../../../crate/pkg/rl_wasm";
 import { NextApiRequest, NextApiResponse } from "next";
-import wasmPath from "../../../crate/pkg/rl_wasm_bg.wasm";
-import samplePath from "../../../dev/sample.replay";
-
-const prefix = process.env.NODE_ENV === "production" ? "chunks/" : "";
+import path from 'path';
 
 async function compileWasm() {
-  const data = await fs.readFile(`./.next/server/${prefix}${wasmPath}`);
+  // https://vercel.com/docs/concepts/functions/serverless-functions/runtimes#including-additional-files
+  const file = path.join(process.cwd(), 'crate', 'pkg', 'rl_wasm_bg.wasm');
+  const data = await fs.readFile(file);
   return WebAssembly.compile(data);
 }
 
@@ -17,7 +16,8 @@ const wasmInit = init(compileWasm());
 export default async function handler(_: NextApiRequest, res: NextApiResponse) {
   await wasmInit;
   const parser = new ReplayParser(RlMod);
-  const fileData = await fs.readFile(`./.next/server/${prefix}${samplePath}`);
+  const file = path.join(process.cwd(), 'dev', 'sample.replay');
+  const fileData = await fs.readFile(file);
   const input = new Uint8Array(fileData);
   const out = parser.parse(input);
   res.status(200).json(out);
