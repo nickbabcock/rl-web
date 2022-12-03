cd01eaacdb19f20a8ad93d49092ace29508e9ac5
Nick Babcock <nbabcock19@hotmail.com>
2022-12-02 21:50:34 -0600

    ...

--- src/pages/api/sample.ts
+++ src/pages/api/sample.ts
@@ -2,15 +2,12 @@ import fs from "fs/promises";
 import { ReplayParser } from "@/features/worker/ReplayParser";
 import init, * as RlMod from "../../../crate/pkg/rl_wasm";
 import { NextApiRequest, NextApiResponse } from "next";
-// import wasmPath from "../../../crate/pkg/rl_wasm_bg.wasm";
-// import samplePath from "../../../dev/sample.replay";
-
-const prefix = process.env.NODE_ENV === "production" ? "chunks/" : "";
-
-console.log("DIR", process.cwd());
+import path from 'path';
 
 async function compileWasm() {
-  const data = await fs.readFile(`../../../crate/pkg/rl_wasm_bg.wasm`);
+  // https://vercel.com/docs/concepts/functions/serverless-functions/runtimes#including-additional-files
+  const file = path.join(process.cwd(), 'crate', 'pkg', 'rl_wasm_bg.wasm');
+  const data = await fs.readFile(file);
   return WebAssembly.compile(data);
 }
 
@@ -18,8 +15,9 @@ const wasmInit = init(compileWasm());
 
 export default async function handler(_: NextApiRequest, res: NextApiResponse) {
   await wasmInit;
-const parser = new ReplayParser(RlMod);
-  const fileData = await fs.readFile(`../../../dev/sample.replay`);
+  const parser = new ReplayParser(RlMod);
+  const file = path.join(process.cwd(), 'dev', 'sample.replay');
+  const fileData = await fs.readFile(file);
   const input = new Uint8Array(fileData);
   const out = parser.parse(input);
   res.status(200).json(out);
