13ff8f82bf39bb325306e4546ee1c2ae58618eed
Nick Babcock <nbabcock19@hotmail.com>
2025-09-18 21:07:00 -0500

    Update test snapshot

--- crate/tests/test.rs
+++ crate/tests/test.rs
@@ -39,7 +39,7 @@ fn test_parse_network_bad() {
 
     let replay = parse(&v).unwrap();
     let err = replay.network_err().unwrap();
-    assert!(err.contains("Error decoding frame: time is out of range:"));
+    assert_eq!(err, "Error decoding frame: attribute unknown or not implemented:\n----------\nactor id: 66\nactor object id: 246\nattribute stream id: 2\nattribute: Engine.Actor:Velocity\n\nCurrent frame: 157 (time: 112.54524 delta: 0.048387136)\n\nLast actor update:\n------------------\nactor id: 66\nobject id: 246\nobject name: Archetypes.Car.Car_Default\nattribute stream id: 51\nattribute object id: 223\nattribute object name: TAGame.RBActor_TA:ReplicatedRBState\nattribute: RigidBody(RigidBody { sleeping: false, location: Vector3f { x: -4.47, y: -8.11, z: 0.19 }, rotation: Quaternion { x: 0.020142216, y: 0.019592883, z: 0.019592883, w: 0.0 }, linear_velocity: Some(Vector3f { x: -0.04, y: -0.07, z: -0.04 }), angular_velocity: Some(Vector3f { x: -0.02, y: -0.03, z: -0.04 }) })\n");
     assert!(replay.header_json(false).contains(r#""header_size":6402,"#));
     assert!(replay.header_json(true).contains(r#""header_size": 6402,"#));
 }
